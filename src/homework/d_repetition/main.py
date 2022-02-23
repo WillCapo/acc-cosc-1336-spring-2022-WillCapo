1053de1a9f62af3512e2824fe686ca0c98cd5011
WillCapo <William.capozzoli@g.austincc.edu>
2022-02-22 21:22:36 -0600

    Homework 2 quick fix

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -5,14 +5,16 @@ choice = int(input('\nHomework 3 Menu\n\n1-Factorial\n2-Sum odd numbers\n3-Exit\
 if(choice == 1):
     input = int(input('\nEnter a number: '))
     if(input < 0 or input > 10):
-        print('\nEnter a number between 0 and 10 next time.')
-    print('\nYour factorial is ' + str(repetition.get_factorial(input)) + '\n')
+        print('\nEnter a number between 0 and 10 next time.\n')
+    else: 
+        print('\nYour factorial is ' + str(repetition.get_factorial(input)) + '\n')
 
 if(choice == 2):
     input = int(input('\nEnter a number: '))
     if(input < 0 or input > 100):
-        print('\nEnter a number between 0 and 100 next time.')
-    print('\nYour total is ' + str(repetition.sum_odd_numbers(input)) + '\n')
+        print('\nEnter a number between 0 and 100 next time.\n')
+    else: 
+        print('\nYour total is ' + str(repetition.sum_odd_numbers(input)) + '\n')
 
 if(choice == 3):
     print('\nExiting menu.\n')
